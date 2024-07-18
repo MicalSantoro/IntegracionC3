3944796e753e66ff34dcb036375fa25cb274de5a
Mical Santoro <mical.santoro@gmail.com>
2024-07-17 23:01:37 -0400

    first commit

---
+++ Eva_1MS/src/main/java/com/iplacex/Eva_1MS/App.java
@@ -0,0 +1,13 @@
+package com.iplacex.Eva_1MS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola Mundo desde Eclipse con Maven" );
+    }
+}
---
+++ Eva_1MS/src/test/java/com/iplacex/Eva_1MS/AppTest.java
@@ -0,0 +1,20 @@
+package com.iplacex.Eva_1MS;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
